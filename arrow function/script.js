318cb4cd11be8ccd1f9e1b2360db02ac55071471
Sijan Panta <cjan1102@gmail.com>
2024-08-17 09:54:56 +0545

    Center the calculator in the Website

---
+++ arrow function/script.js
@@ -0,0 +1,29 @@
+
+// const divs=document.querySelectorAll("div")
+// console.log("divs: ")
+// console.log(divs)
+// console.log("divs[0]: ")
+// console.log(divs[0])
+// console.log("document: ")
+// console.log(document)
+
+
+// // document.addEventListener("click", e =>{
+// //     if (e.target.matches('div')){
+// //     console.log("hi")}
+// // })
+
+// // let fun=()=>console.log("hello")
+// // fun()
+
+// const mylist = [ "value1", "value2"]
+
+for(let i = 0; i < 10 ; i++){
+    var x = 10;
+}
+
+{
+    var five = 5;
+}
+
+
